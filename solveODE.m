66a588a6a1ce31cfb08ea56e65d4e1ccac7f36f1
Uddhava Swaminathan <uddhava2m@gmail.com>
2023-02-27 20:21:29 -0500

    Update solveODE.m

--- solveODE.m
+++ solveODE.m
@@ -1,8 +1,5 @@
 function wsol = solveODE()
-import matlab.*
-import matlab.math.*
-import matlab.graphics.*
-import matlab.io.*
+n = 0.5;
 
 K = 150; % degrees Celsius
 x_f = 0.1;
@@ -10,11 +7,10 @@ E_1 = 1.4; % eV
 A_1 = 1.25 * 10^17;
 k_B = 8.617 * 10^-5;
 T0 = 80+273;
-n = 0.5;
 
 abserr = 1.0e-8;
 relerr = 1.0e-6;
-stoptime = 170;
+stoptime = 166;
 numpoints = 250;
 t = linspace(0, stoptime, numpoints);
 
@@ -28,7 +24,7 @@ plot(wsol.x, wsol.y(1, :));
 figure;
 plot(wsol.x, wsol.y(2, :));
     
-    function f = vectorfield(w, t, p)
+    function f = vectorfield(w, t, p) %#ok<INUSL> 
         x1 = w(1);
         T1 = w(2);
         A_1 = p(1);
