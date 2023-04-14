406a015a0139bf04283795a46a13dcc3c10e39b0
Uddhava Swaminathan <uddhava2m@gmail.com>
2023-04-14 11:54:02 -0400

    fixed

--- BonusScenario/Bonus.m
+++ BonusScenario/Bonus.m
@@ -20,7 +20,6 @@ E_2 = 0.8;
 A_2 = 1e8;
 z0 = 0.15;
 a_L = 1.1;
-a_H = 6.9; %Not used since it causes the model to misbehave
 a = a_L;
 a0 = 1;
 m = 1;
@@ -36,7 +35,6 @@ options = odeset('RelTol',relerr,'AbsTol',abserr, 'NonNegative', 1);
 p = [A_1, E_1, k_B, n, K1, A_2, E_2, m, K2, a, a0, z0];
 %Initial Value Array
 x0 = [x_f; T0; x_i; z0];
-warning('off', 'MATLAB:plot:IgnoreImaginaryXYPart');
 %Solve
 xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
 
@@ -48,20 +46,20 @@ ax2 = subplot(3, 1, 2);
 hold on;
 ax3 = subplot(3, 1, 3);
 hold on;
-plot(ax1, xsol.y(2, :) - 273.15, xsol.y(1, :))
-plot(ax2, xsol.y(2, :) - 273.15, xsol.y(3, :))
-plot(ax3, xsol.y(2, :) - 273.15, xsol.y(4, :))
+plot(ax1, xsol.y(2, :) - 273.15, xsol.y(1, :), 'LineWidth', 3)
+plot(ax2, xsol.y(2, :) - 273.15, xsol.y(3, :), 'LineWidth', 3)
+plot(ax3, xsol.y(2, :) - 273.15, xsol.y(4, :), 'LineWidth', 3)
 
 %Get value of dTdt at temperature T
 for i = 1:length(xsol.y(1, :))
     x = [xsol.y(1, i) xsol.y(2, i) xsol.y(3, i) xsol.y(4, i)];
-    dTdt(:, i) = ODE(0, x, p);
+    dTdt(:, i) = ODE(0, x, p); %#ok<*SAGROW> 
 
 end
 figure;
 ax4 = axes();
 hold on;
-semilogy(ax4, xsol.y(2, :) - 273.15, dTdt(2, :));
+semilogy(ax4, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth', 3);
 
 %Now solving for T_0 = 100
 clear xsol dTdt;
@@ -74,9 +72,9 @@ x0 = [x_f; T0; x_i; z0];
 xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
 
 %Plot
-plot(ax1, xsol.y(2, :) - 273.15, xsol.y(1, :))
-plot(ax2, xsol.y(2, :) - 273.15, xsol.y(3, :))
-plot(ax3, xsol.y(2, :) - 273.15, xsol.y(4, :))
+plot(ax1, xsol.y(2, :) - 273.15, xsol.y(1, :), 'LineWidth', 3)
+plot(ax2, xsol.y(2, :) - 273.15, xsol.y(3, :), 'LineWidth', 3)
+plot(ax3, xsol.y(2, :) - 273.15, xsol.y(4, :), 'LineWidth', 3)
 
 %Get value of dT/dt at temperature T
 for i = 1:length(xsol.y(1, :))
@@ -85,22 +83,22 @@ for i = 1:length(xsol.y(1, :))
 
 end
 
-semilogy(ax4, xsol.y(2, :) - 273.15, dTdt(2, :));
+semilogy(ax4, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth', 3);
 
 %Repeated as prior
 clear xsol dTdt;
 T0 = 115 + 273.15;
 x0 = [x_f; T0; x_i; z0];
 xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
-plot(ax1, xsol.y(2, :) - 273.15, xsol.y(1, :))
-plot(ax2, xsol.y(2, :) - 273.15, xsol.y(3, :))
-plot(ax3, xsol.y(2, :) - 273.15, xsol.y(4, :))
+plot(ax1, xsol.y(2, :) - 273.15, xsol.y(1, :), 'LineWidth', 3)
+plot(ax2, xsol.y(2, :) - 273.15, xsol.y(3, :), 'LineWidth', 3)
+plot(ax3, xsol.y(2, :) - 273.15, xsol.y(4, :), 'LineWidth', 3)
 for i = 1:length(xsol.y(1, :))
     x = [xsol.y(1, i) xsol.y(2, i) xsol.y(3, i) xsol.y(4, i)];
     dTdt(:, i) = ODE(0, x, p);
 
 end
-semilogy(ax4, xsol.y(2, :) - 273.15, dTdt(2, :));
+semilogy(ax4, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth', 3);
 
 %Label and scale plots
 xlim([80 220]);
@@ -119,15 +117,19 @@ legend(ax1, "T0 = " + string([80 100 115]));
 legend(ax2, "T0 = " + string([80 100 115]));
 legend(ax3, "T0 = " + string([80 100 115]));
 legend(ax4, "T0 = " + string([80 100 115]));
-title(ax1, "Low Surface Area");
-title(ax2, "Low Surface Area");
-title(ax3, "Low Surface Area");
-title(ax4, "Low Surface Area");
+title(ax1, "x_f versus Temperature (Fig. 7)");
+title(ax2, "x_i versus Temperature (Fig. 7)");
+title(ax3, "z versus Tempearture (Fig. 7)");
+title(ax4, "dT/dt versus Temperauture (Fig. 7)");
 set(ax4, 'YScale', 'log')
+set(ax1, 'FontSize', 15)
+set(ax2, 'FontSize', 15)
+set(ax3, 'FontSize', 15)
+set(ax4, 'FontSize', 15)
 
 
 %Function of Equations
-function f = ODE(t, x, p)
+function f = ODE(~, x, p)
 
     x_f = x(1);
     T = x(2);
@@ -145,6 +147,10 @@ function f = ODE(t, x, p)
     a = p(10);
     a0 = p(11);
     z0 = p(12);
+
+    if x_f < 0
+        x_f = 0;
+    end
 
     f = [-exp(-E_1/(k_B*T))*A_1*(x_f^n);
         K1*exp(-E_1/(k_B*T))*A_1*(x_f^n) + (K2*A_2*x_i*(a/a0)*exp(-z/z0)*exp(-E_2/(k_B*T)));
--- Scenario5/Scenario5.m
+++ Scenario5/Scenario5.m
@@ -53,32 +53,34 @@ for i = 1:length(A_1_array)
     xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
 
     %Plot xi and temperature
-    plot(ax1, xsol.x, xsol.y(1, :));
-    plot(ax2, xsol.x, xsol.y(2, :) - 273.15);
+    plot(ax1, xsol.x, xsol.y(1, :), 'LineWidth', 2);
+    plot(ax2, xsol.x, xsol.y(2, :) - 273.15, 'LineWidth', 2);
 
     %For each temperature, we resolve the equation to get dT/dt at T
     for j = 1:length(xsol.y(2, :))
         dTdt(:, j) = ODE(0, [xsol.y(1, j) xsol.y(2, j)], p); %#ok<*SAGROW> 
     end
-    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :));
+    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth', 4);
     clear dTdt;
 
 end
 %Plot Setup
 title(ax1, "Dynamics when varying A");
 title(ax2, "Dynamics when varying A");
-xlabel(ax1, "Time");
-ylabel(ax1, "x_f");
-xlabel(ax2, "Time");
-ylabel(ax2, "Temperature (C)");
-legend(ax1, "A = " + string(A_1_array));
-legend(ax2, "A = " + string(A_1_array));
-title(ax3, "Dynamics when varying A");
-xlabel(ax3, "Temperature");
-ylabel(ax3, "dT/dt");
-legend(ax3, "A = " + string(A_1_array));
+xlabel(ax1, "Time (min)", 'FontSize', 20);
+ylabel(ax1, "x_f", 'FontSize', 20);
+xlabel(ax2, "Time (min)", 'FontSize', 20);
+ylabel(ax2, "Temperature (C)", 'FontSize', 20);
+legend(ax1, "A = " + string(A_1_array), 'FontSize', 14, 'Location','best');
+legend(ax2, "A = " + string(A_1_array), 'FontSize', 14, 'Location','best');
+title(ax3, "Dynamics when varying A", 'FontSize', 20);
+xlabel(ax3, "Temperature (C)", 'FontSize', 20);
+ylabel(ax3, "dT/dt", 'FontSize', 20);
+legend(ax3, "A = " + string(A_1_array), 'FontSize', 14, 'Location','best');
 set(ax3, 'YScale', 'log')
 ylim(ax3, [10^-4 10^3])
+set(ax3,"FontSize", 20); set(ax1, "FontSize", 20); set(ax2, "FontSize", 20);
+
 
 %Clear variables for reassigning
 clear xsol ax1 ax2 dTdt ax3;
@@ -97,29 +99,30 @@ hold on;
 for i = 1:length(K_array)
     p = [A_1, E_1, k_B, n, K_array(i)];
     xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
-    plot(ax1, xsol.x, xsol.y(1, :));
-    plot(ax2, xsol.x, xsol.y(2, :) - 273.15);
+    plot(ax1, xsol.x, xsol.y(1, :), 'LineWidth', 2);
+    plot(ax2, xsol.x, xsol.y(2, :) - 273.15, 'LineWidth', 2);
     for j = 1:length(xsol.y(2, :))
         dTdt(:, j) = ODE(0, [xsol.y(1, j) xsol.y(2, j)], p); %#ok<*SAGROW> 
     end
     
-    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :)); clear dTdt;
+    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth',4); clear dTdt;
 end
 
 title(ax1, "Dynamics when varying K");
 title(ax2, "Dynamics when varying K");
-xlabel(ax1, "Time");
-ylabel(ax1, "x_f");
-xlabel(ax2, "Time");
-ylabel(ax2, "Temperature (C)");
-legend(ax1, "K = " + string(K_array));
-legend(ax2, "K = " + string(K_array));
-title(ax3, "Dynamics when varying K");
-xlabel(ax3, "Temperature");
-ylabel(ax3, "dT/dt");
-legend(ax3, "K = " + string(K_array));
+xlabel(ax1, "Time (min)", 'FontSize', 20);
+ylabel(ax1, "x_f", 'FontSize', 20);
+xlabel(ax2, "Time (min)", 'FontSize', 20);
+ylabel(ax2, "Temperature (C)", 'FontSize', 20);
+legend(ax1, "K = " + string(K_array), 'FontSize', 14, 'Location','best');
+legend(ax2, "K = " + string(K_array), 'FontSize', 14, 'Location','best');
+title(ax3, "Dynamics when varying K", 'FontSize', 20);
+xlabel(ax3, "Temperature (C)", 'FontSize', 20);
+ylabel(ax3, "dT/dt", 'FontSize', 20);
+legend(ax3, "K = " + string(K_array), 'FontSize', 14, 'Location','best');
 ylim(ax3, [10^-4 10^3])
 set(ax3, 'YScale', 'log')
+set(ax3,"FontSize", 20); set(ax1, "FontSize", 20); set(ax2, "FontSize", 20);
 
 %Varying E_1
 clear xsol ax1 ax2 dTdt ax3;
@@ -137,29 +140,30 @@ E_1_array = linspace(1.38, E_1*5, 5);
 for i = 1:length(E_1_array)
     p = [A_1, E_1_array(i), k_B, n, K];
     xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
-    plot(ax1, xsol.x, xsol.y(1, :));
-    plot(ax2, xsol.x, xsol.y(2, :) - 273.15);
+    plot(ax1, xsol.x, xsol.y(1, :), 'LineWidth', 2);
+    plot(ax2, xsol.x, xsol.y(2, :) - 273.15, 'LineWidth', 2);
 
     for j = 1:length(xsol.y(2, :))
         dTdt(:, j) = ODE(0, [xsol.y(1, j) xsol.y(2, j)], p);
     end
-    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :)); clear dTdt;
+    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth',4); clear dTdt;
 end
 
 title(ax1, "Dynamics when varying E_1");
 title(ax2, "Dynamics when varying E_1");
-xlabel(ax1, "Time");
-ylabel(ax1, "x_f");
-xlabel(ax2, "Time");
-ylabel(ax2, "Temperature (C)");
-legend(ax1, "E_1 = " + string(E_1_array));
-legend(ax2, "E_1 = " + string(E_1_array));
-title(ax3, "Dynamics when varying E_1");
-xlabel(ax3, "Temperature");
-ylabel(ax3, "dT/dt");
-legend(ax3, "E_1 = " + string(E_1_array));
+xlabel(ax1, "Time (min)", 'FontSize', 20);
+ylabel(ax1, "x_f", 'FontSize', 20);
+xlabel(ax2, "Time (min)", 'FontSize', 20);
+ylabel(ax2, "Temperature (C)", 'FontSize', 20);
+legend(ax1, "E_1 = " + string(E_1_array), 'FontSize', 14, 'Location','best');
+legend(ax2, "E_1 = " + string(E_1_array), 'FontSize', 14, 'Location','best');
+title(ax3, "Dynamics when varying E_1", 'FontSize', 20);
+xlabel(ax3, "Temperature (C)", 'FontSize', 20);
+ylabel(ax3, "dT/dt", 'FontSize', 20);
+legend(ax3, "E_1 = " + string(E_1_array), 'FontSize', 14, 'Location','best');
 ylim(ax3, [10^-4 10^3])
 set(ax3, 'YScale', 'log')
+set(ax3,"FontSize", 20); set(ax1, "FontSize", 20); set(ax2, "FontSize", 20);
 
 %Varying n
 clear xsol ax1 ax2 dTdt ax3;
@@ -177,29 +181,30 @@ hold on;
 for i = 1:length(n_array)
     p = [A_1, E_1, k_B, n_array(i), K];
     xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
-    plot(ax1, xsol.x, xsol.y(1, :));
-    plot(ax2, xsol.x, xsol.y(2, :) - 273.15);
+    plot(ax1, xsol.x, xsol.y(1, :), 'LineWidth', 2);
+    plot(ax2, xsol.x, xsol.y(2, :) - 273.15, 'LineWidth', 2);
 
     for j = 1:length(xsol.y(2, :))
         dTdt(:, j) = ODE(0, [xsol.y(1, j) xsol.y(2, j)], p);
     end
-    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :)); clear dTdt;
+    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth',4); clear dTdt;
 end
 
 title(ax1, "Dynamics when varying n");
 title(ax2, "Dynamics when varying n");
-xlabel(ax1, "Time");
-ylabel(ax1, "x_f");
-xlabel(ax2, "Time");
-ylabel(ax2, "Temperature (C)");
-legend(ax1, "n = " + string(n_array));
-legend(ax2, "n = " + string(n_array));
-title(ax3, "Dynamics when varying n");
-xlabel(ax3, "Temperature");
-ylabel(ax3, "dT/dt");
-legend(ax3, "n = " + string(n_array));
+xlabel(ax1, "Time (min)", 'FontSize', 20);
+ylabel(ax1, "x_f", 'FontSize', 20);
+xlabel(ax2, "Time (min)", 'FontSize', 20);
+ylabel(ax2, "Temperature (C)", 'FontSize', 20);
+legend(ax1, "n = " + string(n_array), 'FontSize', 14, 'Location','best');
+legend(ax2, "n = " + string(n_array), 'FontSize', 14, 'Location','best');
+title(ax3, "Dynamics when varying n", 'FontSize', 20);
+xlabel(ax3, "Temperature (C)", 'FontSize', 20);
+ylabel(ax3, "dT/dt", 'FontSize', 20);
+legend(ax3, "n = " + string(n_array), 'FontSize', 14, 'Location','best');
 ylim(ax3, [10^-4 10^3])
 set(ax3, 'YScale', 'log')
+set(ax3,"FontSize", 20); set(ax1, "FontSize", 20); set(ax2, "FontSize", 20);
 
 
 %Varying x_f
@@ -219,30 +224,31 @@ for i = 1:length(x_f_array)
     p = [A_1, E_1, k_B, n, K];
     x0 = [x_f_array(i), T0];
     xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
-    plot(ax1, xsol.x, xsol.y(1, :));
-    plot(ax2, xsol.x, xsol.y(2, :) - 273.15);
+    plot(ax1, xsol.x, xsol.y(1, :), 'LineWidth', 2);
+    plot(ax2, xsol.x, xsol.y(2, :) - 273.15, 'LineWidth', 2);
     
     for j = 1:length(xsol.y(2, :))
         dTdt(:, j) = ODE(0, [xsol.y(1, j) xsol.y(2, j)], p);
     end
-    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :)); clear dTdt;
+    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth',4); clear dTdt;
 
 end
 
 title(ax1, "Dynamics when varying x_f0");
 title(ax2, "Dynamics when varying x_f0");
-xlabel(ax1, "Time");
-ylabel(ax1, "x_f");
-xlabel(ax2, "Time");
-ylabel(ax2, "Temperature (C)");
-legend(ax1, "x_f = " + string(x_f_array));
-legend(ax2, "x_f = " + string(x_f_array));
-xlabel(ax3, "Temperature");
-title(ax3, "Dynamics when varying x_f0");
-ylabel(ax3, "dT/dt");
-legend(ax3, "x_f = " + string(x_f_array));
+xlabel(ax1, "Time (min)", 'FontSize', 20);
+ylabel(ax1, "x_f", 'FontSize', 20);
+xlabel(ax2, "Time (min)", 'FontSize', 20);
+ylabel(ax2, "Temperature (C)", 'FontSize', 20);
+legend(ax1, "x_f = " + string(x_f_array), 'FontSize', 14, 'Location','best');
+legend(ax2, "x_f = " + string(x_f_array), 'FontSize', 14, 'Location','best');
+xlabel(ax3, "Temperature (C)", 'FontSize', 20);
+title(ax3, "Dynamics when varying x_f0", 'FontSize', 20);
+ylabel(ax3, "dT/dt", 'FontSize', 20);
+legend(ax3, "x_f = " + string(x_f_array), 'FontSize', 14, 'Location','best');
 ylim(ax3, [10^-4 10^3])
 set(ax3, 'YScale', 'log')
+set(ax3,"FontSize", 20); set(ax1, "FontSize", 20); set(ax2, "FontSize", 20);
 
 %Varying T0
 clear xsol ax1 ax2 dTdt ax3;
@@ -260,29 +266,30 @@ for i = 1:length(T0_array)
     p = [A_1, E_1, k_B, n, K];
     x0 = [x_f, T0_array(i)];
     xsol = ode15s(@(t,x)ODE(t, x, p), t, x0, options);
-    plot(ax1, xsol.x, xsol.y(1, :));
-    plot(ax2, xsol.x, xsol.y(2, :) - 273.15);
+    plot(ax1, xsol.x, xsol.y(1, :), 'LineWidth', 2);
+    plot(ax2, xsol.x, xsol.y(2, :) - 273.15, 'LineWidth', 2);
 
     for j = 1:length(xsol.y(2, :))
         dTdt(:, j) = ODE(0, [xsol.y(1, j) xsol.y(2, j)], p);
     end
-    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :)); clear dTdt;
+    semilogy(ax3, xsol.y(2, :) - 273.15, dTdt(2, :), 'LineWidth',4); clear dTdt;
 end
 
 title(ax1, "Dynamics when varying T0");
 title(ax2, "Dynamics when varying T0");
-xlabel(ax1, "Time");
-ylabel(ax1, "x_f");
-xlabel(ax2, "Time");
-ylabel(ax2, "Temperature (C)");
-legend(ax1, "T0 = " + string(T0_array - 273.15));
-legend(ax2, "T0 = " + string(T0_array - 273.15));
-title(ax3, "Dynamics when varying T0");
-xlabel(ax3, "Temperature");
-ylabel(ax3, "dT/dt");
-legend(ax3, "T0 = " + string(T0_array - 273.15));
+xlabel(ax1, "Time (min)", 'FontSize', 20);
+ylabel(ax1, "x_f", 'FontSize', 20);
+xlabel(ax2, "Time (min)", 'FontSize', 20);
+ylabel(ax2, "Temperature (C)", 'FontSize', 20);
+legend(ax1, "T0 = " + string(T0_array - 273.15), 'FontSize', 14, 'Location','best');
+legend(ax2, "T0 = " + string(T0_array - 273.15), 'FontSize', 14, 'Location','best');
+title(ax3, "Dynamics when varying T0", 'FontSize', 20);
+xlabel(ax3, "Temperature (C)", 'FontSize', 20);
+ylabel(ax3, "dT/dt", 'FontSize', 20);
+legend(ax3, "T0 = " + string(T0_array - 273.15), 'FontSize', 14, 'Location','best');
 ylim(ax3, [10^-4 10^3])
 set(ax3, 'YScale', 'log')
+set(ax3,"FontSize", 20); set(ax1, "FontSize", 20); set(ax2, "FontSize", 20);
 
 %Differential Equation Function
 function f = ODE(~, x, p)
